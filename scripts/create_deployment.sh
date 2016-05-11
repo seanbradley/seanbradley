f807d6450271f56d8fb43830bbebfd776daa359d
Sean Bradley <sean@blogblimp.com>
2016-05-11 16:05:01 -0700

    Increment create_deployment.sh.

--- scripts/create_deployment.sh
+++ scripts/create_deployment.sh
@@ -4,8 +4,8 @@ aws deploy create-deployment \
 --application-name wolfskill \
 --deployment-config-name CodeDeployDefault.OneAtATime \
 --deployment-group-name wolfskilldg \
---description "Wolfskill 1.0.2." \
---github-location repository=seanbradley/wolfskill,commitId=7caddbf5ab5f62210bc93a4db52041d55bb2e02f
+--description "Wolfskill 1.0.3." \
+--github-location repository=seanbradley/wolfskill,commitId=2f69d656ed1c4646339f9a692d4b7409f8c2a00c
 
 
 
